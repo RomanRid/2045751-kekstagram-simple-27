c55f061925b588b0a84e210ce26bddc2a9d4e8bb
Роман Боронин <r.vboronin@gmail.com>
2022-11-13 09:24:07 +0300

    добавляет функцию уменьшения масштаба изображения

--- js/scale.js
+++ js/scale.js
@@ -7,3 +7,29 @@ const SCALE_STEP = 25;
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const DEFAULT_SCALE = 100;
+
+
+function changedScaleImage(value = DEFAULT_SCALE) {
+
+  image.style.transform = `scale(${value / 100})`;
+  scaleControl.value = `${value}%`;
+
+}
+
+changedScaleImage();
+
+function onbuttonSmallerClick() {
+
+  const presentValue = parseInt(scaleControl.value, 10);
+
+  let newValue = presentValue - SCALE_STEP;
+  if (newValue < MIN_SCALE) {
+    newValue = MIN_SCALE;
+  }
+
+  changedScaleImage(newValue);
+
+}
+
+
+buttonSmaller.addEventListener('click', onbuttonSmallerClick);
